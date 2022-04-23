ba9cd1dbd983eb5daeaeba373b85fdfc405912c8
Vadim <kalugavadim04@gmail.com>
2022-04-23 16:57:18 +0700

    2D array

--- MPI/MPI.cpp
+++ MPI/MPI.cpp
@@ -1,8 +1,20 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <random>
+#include <malloc.h>
 #include "mpi.h"
 
 #define FIRST_PROCESS 0
+
+/*выделение памяти под двумерный массиав*/
+int** matrix_init(int rows, int cols) {
+	int* ptr = (int*)malloc(rows * cols * sizeof(int));
+	int** matrix = (int**)malloc(rows * sizeof(int*));
+	for (int i = 0; i < rows; i++) {
+		matrix[i] = &(ptr[cols * i]);
+	}
+	return matrix;
+}
 
 int main(int* argc, char** argv) {
 	int process_size, rank;
@@ -13,45 +25,55 @@ int main(int* argc, char** argv) {
 	MPI_Comm_size(MPI_COMM_WORLD, &process_size);	//numtasks := число параллельных процссов
 	MPI_Status status;
 
-	int* buff = NULL;
-	int buff_size;
+	int** matrix = NULL;
+	int matrix_parameters[2];	//высота и ширина матрицы
 
 	if (rank == FIRST_PROCESS) { //если процесс первый
 		printf(" >>> Num threads: %d\n", process_size);
-		printf(" >>> Input mass size: ");
+
+		printf(" >>> Input mass height: ");
 		fflush(stdout);	//очистка буфера
-		int size;
-		scanf("%d", &size);
-	
-		buff = new int[size];
-		/*генерирование массива для пересылки*/
-		for (int i = 0; i < size; i++) {
-			*(buff + i) = i+1;
+		int rows;
+		scanf("%d", &rows);
+		matrix_parameters[0] = rows;
+
+		printf(" >>> Input mass width: ");
+		fflush(stdout);	//очистка буфера
+		int cols;
+		scanf("%d", &cols);
+		matrix_parameters[1] = cols;
+
+		matrix = matrix_init(rows, cols);
+
+		/*заполнение матрицы значениями*/
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				matrix[i][j] = rand() % 9;
+			}
 		}
 
-		printf(" Hello MPI from process = %d, buff = [ ", rank);
-		for (int i = 0; i < size; i++) {
-			printf(" %d", *(buff + i));
+		printf(" Hello MPI from process = %d, matrix: \n", rank);
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				printf(" %d", matrix[i][j]);
+			}
+			printf("\n");
 		}
-		printf(" ]\n");
 
 		/*отправка всем процессам исходных данных с тегом сообщений 1 и 0*/
 		for (int to_process = 1; to_process < process_size; to_process++) {
-			MPI_Send(&size, 1, MPI_INT, to_process, 0, MPI_COMM_WORLD);	//размер массива
-			MPI_Send(buff, size, MPI_INT, to_process, 1, MPI_COMM_WORLD);	//массив
+			MPI_Send(matrix_parameters, 2, MPI_INT, to_process, 0, MPI_COMM_WORLD);	//размер массива
+			MPI_Send(&(matrix[0][0]), rows * cols, MPI_INT, to_process, 1, MPI_COMM_WORLD);	//массив
 		}
 	}
 	else {	//если процесс не первый, то ожидаем получения данных
-
 		/*получение размера данных и выделение под них памяти*/
-		MPI_Recv(&buff_size, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-		buff = new int[buff_size];
+		MPI_Recv(matrix_parameters, 2, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+		matrix = matrix_init(matrix_parameters[0], matrix_parameters[1]);
 
 		/*получение исходных данных*/
-		MPI_Recv(buff, buff_size, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
-		printf(" message form %d process, buff[7] = %d\n", status.MPI_SOURCE, *(buff+7));
+		MPI_Recv(&(matrix[0][0]), matrix_parameters[0] * matrix_parameters[1], MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
 	}
-
 
 	MPI_Finalize();	//завершение параллельной части программы
 
