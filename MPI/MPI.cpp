0fd78cc45743003a7e91b1e731f1d068f19776b8
Vadim <kalugavadim04@gmail.com>
2022-04-23 23:27:38 +0700

    added algorithm

--- MPI/MPI.cpp
+++ MPI/MPI.cpp
@@ -16,6 +16,36 @@ int** matrix_init(int rows, int cols) {
 	return matrix;
 }
 
+/*выборка элементовы выше побочной диагонали*/
+int* mpi_above_side_diagonal(int rank, int process_size, int** matrix, int n) {
+	int size = (n * n - n) / 2;
+	int* arr = (int*)malloc(size * sizeof(int));
+	/*обнуление массива*/
+	for (int i = 0; i < size; i++) {
+		arr[i] = 0;
+	}
+
+	int interval = (int)(n / (process_size - 1));	//разбиение массива между процессами
+	for (int i = (rank - 1) * interval; i < (rank - 1) * interval + interval && i < n; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			int counter = 0;
+			int t = n;
+			/*вычисление "своего места" в будующем массиве*/
+			for (int k = 0; k < i; k++) {
+				t = t - 1;
+				counter += t;
+			}
+			arr[j + counter] = matrix[i][j];
+		}
+		/*особое уловие для предпоследней строки для некоторых матриц*/
+		if (i == (process_size - 2) * interval) {
+			arr[size - 1] = matrix[n - 2][0];
+		}
+	}
+
+	return arr;
+}
+
 int main(int* argc, char** argv) {
 	int process_size, rank;
 
@@ -27,6 +57,9 @@ int main(int* argc, char** argv) {
 
 	int** matrix = NULL;
 	int matrix_parameters[2];	//высота и ширина матрицы
+	int* result = NULL;
+	int result_size = 0;
+	int* arr = NULL;	//разобщённые резльтаты работы процессов
 
 	if (rank == FIRST_PROCESS) { //если процесс первый
 		printf(" >>> Num threads: %d\n", process_size);
@@ -44,18 +77,24 @@ int main(int* argc, char** argv) {
 		matrix_parameters[1] = cols;
 
 		matrix = matrix_init(rows, cols);
+		result_size = (rows * rows - rows) / 2;
+		result = (int*)malloc(result_size*sizeof(int));
+		for (int i = 0; i < result_size; i++) {
+			result[i] = 0;
+		}
 
 		/*заполнение матрицы значениями*/
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
-				matrix[i][j] = rand() % 9;
+				//matrix[i][j] = rand() % 9 + 1;
+				matrix[i][j] = (i + 1) * (j + 1);
 			}
 		}
 
 		printf(" Hello MPI from process = %d, matrix: \n", rank);
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
-				printf(" %d", matrix[i][j]);
+				printf("\t%d", matrix[i][j]);
 			}
 			printf("\n");
 		}
@@ -65,6 +104,21 @@ int main(int* argc, char** argv) {
 			MPI_Send(matrix_parameters, 2, MPI_INT, to_process, 0, MPI_COMM_WORLD);	//размер массива
 			MPI_Send(&(matrix[0][0]), rows * cols, MPI_INT, to_process, 1, MPI_COMM_WORLD);	//массив
 		}
+
+		arr = (int*)malloc(result_size * sizeof(int));
+		for (int from_process = 2; from_process <= process_size; from_process++) {
+			MPI_Recv(arr, result_size, MPI_INT, MPI_ANY_SOURCE, from_process, MPI_COMM_WORLD, &status);
+			for (int i = 0; i < result_size; i++) {
+				result[i] += arr[i];
+			}
+		}
+
+		printf("\n >>> RESULT: [ ");
+		for (int i = 0; i < result_size; i++) {
+			printf(" %d", result[i]);
+		}
+		printf(" ]\n");
+
 	}
 	else {	//если процесс не первый, то ожидаем получения данных
 		/*получение размера данных и выделение под них памяти*/
@@ -73,9 +127,24 @@ int main(int* argc, char** argv) {
 
 		/*получение исходных данных*/
 		MPI_Recv(&(matrix[0][0]), matrix_parameters[0] * matrix_parameters[1], MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+		result_size = ((matrix_parameters[0] * matrix_parameters[0] - matrix_parameters[0]) / 2);
+
+		arr = mpi_above_side_diagonal(rank, process_size, matrix, matrix_parameters[0]);
+		printf("\n rank: %d, arr: [ ", rank);
+		for (int i = 0; i < result_size; i++) {
+			printf(" %d", arr[i]);
+		}
+		printf(" ]\n");
+		//отправка результата работы процесса в основной с тегами от 2 до 4 (в данном случе)
+		MPI_Send(arr, result_size, MPI_INT, 0, rank + 1, MPI_COMM_WORLD);
 	}
 
 	MPI_Finalize();	//завершение параллельной части программы
+
+	/*освобождение выделенной памяти*/
+	free(matrix);
+	free(result);
+	free(arr);
 
 	return 0;
 }
